09a3a99ba8e5a25997bcec3f64cb606e9c3339de
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-23 21:04:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,23 +10,18 @@ def key_for_min_value(name_hash)
     name_hash.each {|key, value|
       values << value
     }
-    puts values
     smallest = values[0]
     for i in 1..(values.length-1)
       if values[i] < smallest
         smallest = values[i]
       end
     end
-    puts smallest
     name_hash.each {|key, value|
       if value == smallest
         the_key = key
       end
     }
-    puts the_key
+    the_key
   end
 end
 
-name_hash = { "mera" => 5, "rayna" => 2}
-
-key_for_min_value(name_hash)
